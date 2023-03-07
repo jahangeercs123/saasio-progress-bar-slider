2dd5c0d9dc2cbfc93f52632381ba03bd60fd30f9
jahangeercs123 <jahangeerahmad26@gmail.com>
2023-03-08 02:16:47 +0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$('.cards-slider').slick({
+    slidesToShow: 4,
+    dots: true,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+});
+$('.slider-row').slick({
+    slidesToShow: 3,
+    dots: true,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+});
